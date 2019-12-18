d117e18669b04b93e0faa4458a454bf5f5ddc2b5
mceme <manuelcostapnf@hotmail.com>
2019-12-18 19:22:14 +0000

    Update sentinel.py

--- bin/sentinel.py
+++ bin/sentinel.py
@@ -22,21 +22,21 @@ import argparse
 
 
 # sync dashd gobject list with our local relational DB backend
-def perform_dashd_object_sync(dashd):
-    GovernanceObject.sync(dashd)
+def perform_dashd_object_sync(ImageCoind):
+    GovernanceObject.sync(ImageCoind)
 
 
 # delete old watchdog objects, create new when necessary
-def watchdog_check(dashd):
+def watchdog_check(ImageCoind):
     printdbg("in watchdog_check")
 
     # delete expired watchdogs
-    for wd in Watchdog.expired(dashd):
+    for wd in Watchdog.expired(ImageCoind):
         printdbg("\tFound expired watchdog [%s], voting to delete" % wd.object_hash)
-        wd.vote(dashd, VoteSignals.delete, VoteOutcomes.yes)
+        wd.vote(ImageCoind, VoteSignals.delete, VoteOutcomes.yes)
 
     # now, get all the active ones...
-    active_wd = Watchdog.active(dashd)
+    active_wd = Watchdog.active(ImageCoind)
     active_count = active_wd.count()
 
     # none exist, submit a new one to the network
@@ -44,7 +44,7 @@ def watchdog_check(dashd):
         # create/submit one
         printdbg("\tNo watchdogs exist... submitting new one.")
         wd = Watchdog(created_at=int(time.time()))
-        wd.submit(dashd)
+        wd.submit(ImageCoind)
 
     else:
         wd_list = sorted(active_wd, key=lambda wd: wd.object_hash)
@@ -52,35 +52,35 @@ def watchdog_check(dashd):
         # highest hash wins
         winner = wd_list.pop()
         printdbg("\tFound winning watchdog [%s], voting VALID" % winner.object_hash)
-        winner.vote(dashd, VoteSignals.valid, VoteOutcomes.yes)
+        winner.vote(ImageCoind, VoteSignals.valid, VoteOutcomes.yes)
 
         # if remaining Watchdogs exist in the list, vote delete
         for wd in wd_list:
             printdbg("\tFound losing watchdog [%s], voting DELETE" % wd.object_hash)
-            wd.vote(dashd, VoteSignals.delete, VoteOutcomes.yes)
+            wd.vote(ImageCoind, VoteSignals.delete, VoteOutcomes.yes)
 
     printdbg("leaving watchdog_check")
 
 
-def prune_expired_proposals(dashd):
+def prune_expired_proposals(ImageCoind):
     # vote delete for old proposals
-    for proposal in Proposal.expired(dashd.superblockcycle()):
-        proposal.vote(dashd, VoteSignals.delete, VoteOutcomes.yes)
+    for proposal in Proposal.expired(ImageCoind.superblockcycle()):
+        proposal.vote(ImageCoind, VoteSignals.delete, VoteOutcomes.yes)
 
 
 # ping dashd
-def sentinel_ping(dashd):
+def sentinel_ping(ImageCoind):
     printdbg("in sentinel_ping")
 
-    dashd.ping()
+    ImageCoind.ping()
 
     printdbg("leaving sentinel_ping")
 
 
-def attempt_superblock_creation(dashd):
+def attempt_superblock_creation(ImageCoind):
     import dashlib
 
-    if not dashd.is_masternode():
+    if not ImageCoind.is_masternode():
         print("We are not a Masternode... can't submit superblocks!")
         return
 
@@ -91,7 +91,7 @@ def attempt_superblock_creation(dashd):
     # has this masternode voted on *any* superblocks at the given event_block_height?
     # have we voted FUNDING=YES for a superblock for this specific event_block_height?
 
-    event_block_height = dashd.next_superblock_height()
+    event_block_height = ImageCoind.next_superblock_height()
 
     if Superblock.is_voted_funding(event_block_height):
         # printdbg("ALREADY VOTED! 'til next time!")
@@ -99,18 +99,18 @@ def attempt_superblock_creation(dashd):
         # vote down any new SBs because we've already chosen a winner
         for sb in Superblock.at_height(event_block_height):
             if not sb.voted_on(signal=VoteSignals.funding):
-                sb.vote(dashd, VoteSignals.funding, VoteOutcomes.no)
+                sb.vote(ImageCoind, VoteSignals.funding, VoteOutcomes.no)
 
         # now return, we're done
         return
 
-    if not dashd.is_govobj_maturity_phase():
+    if not ImageCoind.is_govobj_maturity_phase():
         printdbg("Not in maturity phase yet -- will not attempt Superblock")
         return
 
-    proposals = Proposal.approved_and_ranked(proposal_quorum=dashd.governance_quorum(), next_superblock_max_budget=dashd.next_superblock_max_budget())
-    budget_max = dashd.get_superblock_budget_allocation(event_block_height)
-    sb_epoch_time = dashd.block_height_to_epoch(event_block_height)
+    proposals = Proposal.approved_and_ranked(proposal_quorum=ImageCoind.governance_quorum(), next_superblock_max_budget=ImageCoind.next_superblock_max_budget())
+    budget_max = ImageCoind.get_superblock_budget_allocation(event_block_height)
+    sb_epoch_time = ImageCoind.block_height_to_epoch(event_block_height)
 
     sb = dashlib.create_superblock(proposals, event_block_height, budget_max, sb_epoch_time)
     if not sb:
@@ -120,12 +120,12 @@ def attempt_superblock_creation(dashd):
     # find the deterministic SB w/highest object_hash in the DB
     dbrec = Superblock.find_highest_deterministic(sb.hex_hash())
     if dbrec:
-        dbrec.vote(dashd, VoteSignals.funding, VoteOutcomes.yes)
+        dbrec.vote(ImageCoind, VoteSignals.funding, VoteOutcomes.yes)
 
         # any other blocks which match the sb_hash are duplicates, delete them
         for sb in Superblock.select().where(Superblock.sb_hash == sb.hex_hash()):
             if not sb.voted_on(signal=VoteSignals.funding):
-                sb.vote(dashd, VoteSignals.delete, VoteOutcomes.yes)
+                sb.vote(ImageCoind, VoteSignals.delete, VoteOutcomes.yes)
 
         printdbg("VOTED FUNDING FOR SB! We're done here 'til next superblock cycle.")
         return
@@ -133,24 +133,24 @@ def attempt_superblock_creation(dashd):
         printdbg("The correct superblock wasn't found on the network...")
 
     # if we are the elected masternode...
-    if (dashd.we_are_the_winner()):
+    if (ImageCoind.we_are_the_winner()):
         printdbg("we are the winner! Submit SB to network")
-        sb.submit(dashd)
+        sb.submit(ImageCoind)
 
 
-def check_object_validity(dashd):
+def check_object_validity(ImageCoind):
     # vote (in)valid objects
     for gov_class in [Proposal, Superblock]:
         for obj in gov_class.select():
-            obj.vote_validity(dashd)
+            obj.vote_validity(ImageCoind)
 
 
-def is_dashd_port_open(dashd):
+def is_dashd_port_open(ImageCoind):
     # test socket open before beginning, display instructive message to MN
     # operators if it's not
     port_open = False
     try:
-        info = dashd.rpc_command('getgovernanceinfo')
+        info = ImageCoind.rpc_command('getgovernanceinfo')
         port_open = True
     except (socket.error, JSONRPCException) as e:
         print("%s" % e)
@@ -159,20 +159,20 @@ def is_dashd_port_open(dashd):
 
 
 def main():
-    dashd = ImageCoinDaemon.from_imagecoin_conf(config.imagecoin_conf)
+    ImageCoind = ImageCoinDaemon.from_imagecoin_conf(config.imagecoin_conf)
 
     # check dashd connectivity
-    if not is_dashd_port_open(dashd):
+    if not is_dashd_port_open(ImageCoind):
         print("Cannot connect to ImageCoind. Please ensure ImageCoind is running and the JSONRPC port is open to Sentinel.")
         return
 
     # check dashd sync
-    if not dashd.is_synced():
+    if not ImageCoind.is_synced():
         print("ImageCoind not synced with network! Awaiting full sync before running Sentinel.")
         return
 
     # ensure valid masternode
-    if not dashd.is_masternode():
+    if not ImageCoind.is_masternode():
         print("Invalid Masternode Status, cannot continue.")
         return
 
@@ -198,22 +198,22 @@ def main():
     # ========================================================================
     #
     # load "gobject list" rpc command data, sync objects into internal database
-    perform_dashd_object_sync(dashd)
+    perform_dashd_object_sync(ImageCoind)
 
-    if dashd.has_sentinel_ping:
-        sentinel_ping(dashd)
+    if ImageCoind.has_sentinel_ping:
+        sentinel_ping(ImageCoind)
     else:
         # delete old watchdog objects, create a new if necessary
-        watchdog_check(dashd)
+        watchdog_check(ImageCoind)
 
     # auto vote network objects as valid/invalid
     # check_object_validity(dashd)
 
     # vote to delete expired proposals
-    prune_expired_proposals(dashd)
+    prune_expired_proposals(ImageCoind)
 
     # create a Superblock if necessary
-    attempt_superblock_creation(dashd)
+    attempt_superblock_creation(ImageCoind)
 
     # schedule the next run
     Scheduler.schedule_next_run()
